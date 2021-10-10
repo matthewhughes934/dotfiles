27f3dd9ad6f450824a9839776d9980fa6e49d5ba
Matthew Hughes <matthewhughes934@gmail.com>
2021-10-10 20:12:29 +0100

    Update neomuttconfig aliases
    Update these to be rooted in $HOME rather than $HOME/.config

--- .bashrc.d/dotfiles.sh
+++ .bashrc.d/dotfiles.sh
@@ -2,5 +2,5 @@
 
 alias dotfiles="git --git-dir=$HOME/.dotfiles/dotfiles --work-tree=$HOME"
 alias i3config="git --git-dir=$HOME/.dotfiles/i3 --work-tree=$HOME"
-alias neomuttconfig="git --git-dir=$HOME/.dotfiles/neomuttcfg --work-tree=$HOME/.config/neomutt"
-alias neomuttconfigpriv="git --git-dir=$HOME/.dotfiles/neomuttcfgpriv --work-tree=$HOME/.config/neomutt"
+alias neomuttconfig="git --git-dir=$HOME/.dotfiles/neomuttcfg --work-tree=$HOME"
+alias neomuttconfigpriv="git --git-dir=$HOME/.dotfiles/neomuttcfgpriv --work-tree=$HOME"
