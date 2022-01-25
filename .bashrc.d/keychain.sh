f09391a66736c5646e99baa44d678acd184e4dcd
Matthew Hughes <matthewhughes934@gmail.com>
2022-01-25 19:50:58 +0000

    Remove use of keychain for ssh unlocking
    I realised what I was trying to accomplish is just what `ssh-add` offers
    by default...

--- .bashrc.d/keychain.sh
+++ .bashrc.d/keychain.sh
@@ -1,27 +1,7 @@
 #!/bin/bash
 
-load_keychain () {
-    # from keychain(1) under EXAMPLES
-    if [ -z "$HOSTNAME" ]
-    then
-        HOSTNAME="$(uname -n)"
-    fi
-
-    if [ -f "$HOME/.keychain/$HOSTNAME-sh" ]
-    then
-        # shellcheck disable=SC1090
-        source "$HOME/.keychain/$HOSTNAME-sh"
-    fi
-
-    if [ -f "$HOME/.keychain/$HOSTNAME-sh-gpg" ]
-    then
-        # shellcheck disable=SC1090
-        source "$HOME/.keychain/$HOSTNAME-sh-gpg"
-    fi
-}
-
 unlock_key () {
-    keychain --timeout 30 --quiet "$@"
+    ssh-add -t 1800 "$HOME/.ssh/$@"
 }
 
 _complete_unlock_key () {
