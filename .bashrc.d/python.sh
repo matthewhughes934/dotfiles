1ab0f328ac83d2b184ae21a2d7ee913fb4e705c8
Matthew Hughes <matthewhughes934@gmail.com>
2022-07-13 22:02:11 +0100

    Add alias to load python virtualenv

--- .bashrc.d/python.sh
+++ .bashrc.d/python.sh
@@ -4,3 +4,8 @@ function pymkvenv {
     local path="${1:-.venv}"
     python -m venv .venv "$path" && source "$path/bin/activate"
 }
+
+function pyloadenv {
+    local path="${1:-.venv}"
+    source "$path/bin/activate"
+}
