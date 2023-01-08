0d7dec549cb9b6b89d0d829e514874ec8cac9f38
Matthew Hughes <matthewhughes934@gmail.com>
2023-01-08 23:25:39 +0000

    Fix extra venv created with `pymkvenv`

--- .bashrc.d/python.sh
+++ .bashrc.d/python.sh
@@ -2,7 +2,7 @@
 
 function pymkvenv {
     local path="${1:-.venv}"
-    python -m venv .venv "$path" && source "$path/bin/activate"
+    python -m venv "$path" && source "$path/bin/activate"
 }
 
 function pyloadenv {
