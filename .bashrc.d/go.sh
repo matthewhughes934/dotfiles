cb96ea341f5fa002481a532fe5a2902c48784ee2
Matthew Hughes <matthewhughes934@gmail.com>
2023-01-24 19:54:19 +0000

    Add command to display Go coverage
    Because opening a browser really is unnecessary when I just want to see
    a couple of lines...

---
+++ .bashrc.d/go.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+gocov() {
+    local filename="${1:-coverage.out}"
+    # based on some undocumented behaviour...
+    # https://github.com/golang/go/blob/ffbd194f5cf399a98578ea950c969d07a751ecfd/src/cmd/vendor/golang.org/x/tools/cover/profile.go#L58
+    awk 'NR>1 && $NF==0 {print $1}' "$filename"
+}
