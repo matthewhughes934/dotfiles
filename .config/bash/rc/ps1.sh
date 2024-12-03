02b7f7e9bc30b1c3e872fad2247e194a81a3a585
Matthew Hughes <matthewhughes934@gmail.com>
2024-12-03 08:55:11 +0000

    Update PS1 to show exit status of last command

--- .config/bash/rc/ps1.sh
+++ .config/bash/rc/ps1.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
-white="\033[37;0m"
-bold_cyan="\033[36;1m"
-PS1="\[$bold_cyan\]\$PWD\[$white\]$ "
+PROMPT_COMMAND=__prompt_command
 
-if [ -n "${SSH_TTY:-}" ]
-then
-    PS1="\u@\H:$PS1"
-fi
+__prompt_command() {
+    # must be first line, capture exit of command we just ran
+    local LAST_EXIT="$?"
+
+    if [ "$LAST_EXIT" -eq 0 ]
+    then
+        PS1="✓"
+    else
+        PS1="✗"
+    fi
+
+    local white="\033[37;0m"
+    local bold_cyan="\033[36;1m"
+    PS1+="\[$bold_cyan\]\$PWD\[$white\]$ "
+
+    # add user@host to make it clear we've SSH'd
+    if [ -n "${SSH_TTY:-}" ]
+    then
+        PS1="\u@\H:$PS1"
+    fi
+}
