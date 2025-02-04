9de0f97684651c19fb156f80c0c0c4ddd43758e2
Matthew Hughes <matthewhughes934@gmail.com>
2025-02-04 21:17:32 +0000

    PS1: Better handing of things prefixing PS1
    Basically extending what I was doing for the exit code thing but to be
    more general: we want to persist anything that was prefixed on the PS1,
    but avoid duplicating our custom PS1 changes

--- .config/bash/rc/ps1.sh
+++ .config/bash/rc/ps1.sh
@@ -2,18 +2,29 @@
 
 PS1=""
 PROMPT_COMMAND=(
+    __prompt_setup
     __prompt_suffix_exit_code
     __prompt_suffix_path
     __prompt_suffix_ssh
     __prompt_suffix
 )
 
+__prompt_setup() {
+    # use a delimiter to separate our PS1 changes from anything else
+    # e.g. in a python virtual env we have '(.venv) <our PS1>'
+    local ps1_delim
+    # delimiter: ASCII unit separator
+    ps1_delim="$(printf "\x1f")"
+    # trim everything after the last delim instance (i.e. our PS1 changes)
+    PS1="${PS1%"$ps1_delim"*}"
+    # add the delim instance
+    PS1+="$ps1_delim"
+}
+
 __prompt_suffix_exit_code() {
     # must be first line, capture exit of command we just ran
     local LAST_EXIT="$?"
 
-    # keep any prefix e.g. from python's venv
-    PS1="${PS1%%[✓✗]*}"
     if [ "$LAST_EXIT" -eq 0 ]
     then
         PS1+="✓"
