44048682577581482ea430fc29d7ec64830343d8
Matthew Hughes <matthewhughes934@gmail.com>
2026-03-11 20:16:33 +0000

    nvim: invoke `rust-analyzer` for the current toolchain
    As opposed to invoking the one from `rustup` because if the two are not
    the same I found things like `cargo` would incorrectly detect file
    changes and e.g. rebuild entire projects on a change to a test file

--- .config/nvim/init.d/lspconfig.lua
+++ .config/nvim/init.d/lspconfig.lua
@@ -8,8 +8,19 @@ vim.lsp.config('clangd', {
 })
 
 -- needs: https://github.com/rust-lang/rust-analyzer
+function rust_sysroot()
+    local cmd = { "rustc", "--print", "sysroot" }
+    local output = vim.system(cmd, { text = true }):wait()
+
+    if output.code ~= 0 then
+        vim.notify(('[rust_analzyer] cmd failed with code %d: %s\n%s"'):format(output.code, cmd, output.stderr))
+    end
+    return vim.trim(output.stdout)
+end
+
 vim.lsp.enable('rust_analyzer')
 vim.lsp.config('rust_analyzer', {
+    cmd = { (rust_sysroot() .. "/bin" .. "/rust-analyzer") },
     settings = {
         ['rust-analyzer'] = {
             checkOnSave = {
